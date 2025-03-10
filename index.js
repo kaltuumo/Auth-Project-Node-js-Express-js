ccc54fca81c9ee93bf8323c726a7acac732cf89c
kaltuumo <kaltuumo906@gmail.com>
2025-03-10 10:38:51 +0300

    Back End Node js Express js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const authRouter = require('./routers/authRouter');
+const postRouter = require('./routers/postsRouter');
+
+require('dotenv').config();
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB', err);   
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
+
+app.get('/', (req, res) => {
+    res.send({message:'Hello From The serversssss!'});
+});
+
+// Default to port 8000 if process.env.PORT is not defined
+const port = process.env.PORT || 7001;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ middlewares/validator.js
@@ -0,0 +1,65 @@
+const joi = require('joi');
+
+exports.signupschema = joi.object({
+    email: joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+        tlds: { allow: ['com', 'net'] }
+    }),
+    password: joi.string()
+    .required()
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+
+exports.signinschema = joi.object({
+    email: joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+        tlds: { allow: ['com', 'net'] }
+    }),
+    password: joi.string()
+    .required()
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+exports.acceptCodeSchema = joi.object({
+	email: joi.string()
+		.min(6)
+		.max(60)
+		.required()
+		.email({
+			tlds: { allow: ['com', 'net'] },
+		}),
+	providedCode: joi.number().required(),
+});
+
+exports.changePasswordSchema = joi.object({
+	newPassword: joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+	oldPassword: joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+exports.acceptFPCodeSchema = joi.object({
+	email: joi.string()
+		.min(6)
+		.max(60)
+		.required()
+		.email({
+			tlds: { allow: ['com', 'net'] },
+		}),
+	providedCode: joi.number().required(),
+	newPassword: joi.string()
+		.required()
+		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+});
+
+exports.createPostSchema = joi.object({
+	title: joi.string().min(3).max(60).required(),
+	description: joi.string().min(3).max(600).required(),
+	userId: joi.string().required(),
+});
